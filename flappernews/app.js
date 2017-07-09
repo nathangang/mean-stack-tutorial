dc646bcadb2308c56ec73ccda626558529eb50be
nathangang <nathan.gang.85@gmail.com>
2017-07-09 11:38:15 -0600

    Modified to display a simple collection of post strings.

--- flappernews/app.js
+++ flappernews/app.js
@@ -4,5 +4,13 @@ app.controller('MainCtrl', [
     '$scope',
     function($scope){
         $scope.test = 'Hello World!';
+        $scope.posts = 
+        [
+            'post 1',
+            'post 2',
+            'post 3',
+            'post 4',
+            'post 5'
+        ];
     }
 ]);
