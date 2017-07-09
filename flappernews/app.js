92333078ec620a38ad8a52df00346dc2d6d9e1f9
nathangang <nathan.gang.85@gmail.com>
2017-07-09 15:58:45 -0600

    Added optional use of links when posting.

--- flappernews/app.js
+++ flappernews/app.js
@@ -19,8 +19,9 @@ app.controller('MainCtrl', [
                 return;
             }
 
-            $scope.posts.push({title: $scope.title, upvotes: 0});
+            $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
             $scope.title = '';
+            $scope.link = '';
         }
         $scope.incrementUpvotes = function(post)
         {
