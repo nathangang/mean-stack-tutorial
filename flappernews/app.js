dbd52ac54aa79082b8e33d22d6eae24bbb908c60
nathangang <nathan.gang.85@gmail.com>
2017-07-09 17:54:31 -0600

    Added Posts Controller and a posts state/page.  Added support to add comments to each post.

--- flappernews/app.js
+++ flappernews/app.js
@@ -10,6 +10,11 @@ app.config([
                 url: '/home', 
                 templateUrl: '/home.html', 
                 controller: 'MainCtrl'
+            })
+            .state('posts', {
+                url: '/posts/{id}',
+                templateUrl: '/posts.html',
+                controller: 'PostsCtrl'
             });
             
         $urlRouterProvider.otherwise('home');
@@ -37,7 +42,14 @@ app.controller('MainCtrl', [
                 return;
             }
 
-            $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+            $scope.posts.push({
+                title: $scope.title, 
+                link: $scope.link, 
+                upvotes: 0,
+                comments: [
+                    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                ]});
             $scope.title = '';
             $scope.link = '';
         }
@@ -46,4 +58,34 @@ app.controller('MainCtrl', [
             post.upvotes += 1;
         }
     }
+]);
+
+app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts) {
+        $scope.post = posts.posts[$stateParams.id];
+
+        $scope.addComment = function()
+        {
+            if($scope.body === '')
+            {
+                return;
+            }
+            $scope.post.comments.push(
+                {
+                    body: $scope.body,
+                    author: 'user',
+                    upvotes: 0
+                }
+            );
+            $scope.body = '';
+        }
+        $scope.incrementUpvotes = function(comment)
+        {
+            comment.upvotes += 1;
+        }
+    }
+    
 ]);
